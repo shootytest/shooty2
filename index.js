a80d20bf6793839cb4c6a23aaf185f0f42bae48f
shootytest <shootytest@gmail.com>
2024-11-04 17:23:00 +0800

    doesn't matter

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-import { World } from "./matter.js";
-console.log(World);
+import { Engine } from "./matter.js";
+import { camera } from "./util/camera.js";
+import { init_canvas } from "./util/canvas.js";
+import { key, mouse } from "./util/key.js";
+export const engine = Engine.create();
+export const world = engine.world;
+console.log(world);
+const init_all = () => {
+    init_canvas();
+    // ui.init();
+    key.init();
+};
+const tick_all = () => {
+    // ui.tick();
+    // ui.draw();
+    camera.tick();
+    mouse.tick();
+    requestAnimationFrame(tick_all);
+};
+requestAnimationFrame(tick_all);
+window.addEventListener("load", init_all);
--- index.ts
+++ index.ts
@@ -1,3 +1,29 @@
-import { World } from "./matter.js";
+import { Engine, World } from "./matter.js";
+import { camera } from "./util/camera.js";
+import { ctx, init_canvas, view } from "./util/canvas.js";
+import { key, keys, mouse } from "./util/key.js";
 
-console.log(World);
+export const engine = Engine.create();
+export const world = engine.world;
+
+console.log(world);
+
+const init_all = () => {
+  init_canvas();
+  // ui.init();
+  key.init();
+};
+
+const tick_all = () => {
+
+  // ui.tick();
+  // ui.draw();
+  camera.tick();
+  mouse.tick();
+
+  requestAnimationFrame(tick_all);
+
+};
+
+requestAnimationFrame(tick_all);
+window.addEventListener("load", init_all);
